983f5c87f0b2b2eaf833bbaaaf271989b2b4cc82
Taras Kolodny <trskldn@gmail.com>
2016-05-25 15:20:57 +0300

    initial commit

---
+++ kombiner.js
@@ -0,0 +1,12 @@
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.BackboneMultiExtend = factory();
+  }
+}(this, function() {
+
+  return function() {};  
+}));
